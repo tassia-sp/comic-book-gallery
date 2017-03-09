04e0a43efe478b5cb49c7fce55fe700d29a3a42c
Tassia <tassiapaschoal@gmail.com>
2017-03-08 16:35:11 -0800

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,7 +10,20 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-           return View();
+           //load data (or html) into the controller, not the view. The view should only carry the template and the controller will insert the data)
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Volors: Edgar delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+            return View();
         }
     }
 }
